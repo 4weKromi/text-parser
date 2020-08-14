b928fbbc0975b668553bc493b43a6792511b9ca9
Kromi <33200410+4weKromi@users.noreply.github.com>
2020-08-14 22:31:07 +0530

    Update main.cpp

--- src/testbed/main.cpp
+++ src/testbed/main.cpp
@@ -12,12 +12,12 @@ int main(int argc, char **argv){
 		//app.showBuffer(); //Displays raw data loaded from file
 		if(app.dataParse()){
 			//app.showFormattedDataAll(); //Displays stored vector
-			cout<<app.getConfigKey(1);
-			cout<<" = ";
-			cout<<app.getConfigValue(1)<<endl;
-			cout<<app.getConfigKey(2);
-			cout<<" = ";
-			cout<<app.getConfigValue(2)<<endl;
+			std::cout<<app.getConfigKey(1);
+			std::cout<<" = ";
+			std::cout<<app.getConfigValue(1)<<endl;
+			std::cout<<app.getConfigKey(2);
+			std::cout<<" = ";
+			std::cout<<app.getConfigValue(2)<<endl;
 		}
 	}
 	return 0;
